3436e662810d9c9216f927953ea841e360285565
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-08-27 23:42:51 +0900

    질문리스트 네비바

--- Farmily/Farmily/Screen/QuestionList/QuestionListViewController.swift
+++ Farmily/Farmily/Screen/QuestionList/QuestionListViewController.swift
@@ -7,23 +7,10 @@
 
 import UIKit
 
-class QuestionListViewController: UIViewController {
+final class QuestionListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
