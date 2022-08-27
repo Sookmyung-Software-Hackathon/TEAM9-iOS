9ef7711c69161f8f61f1a25cc0583c150608172a
hellozo0 <ahju0011@gmail.com>
2022-08-28 04:56:52 +0900

    [CHORE] AnswerQuestion UI

--- Farmily/Farmily/Screen/AnswerQuestion/AnswerQuestionViewController.swift
+++ Farmily/Farmily/Screen/AnswerQuestion/AnswerQuestionViewController.swift
@@ -9,21 +9,58 @@ import UIKit
 
 class AnswerQuestionViewController: UIViewController {
 
-    override func viewDidLoad() {
+	let textViewPlaceHolder = "이곳을 눌러서 오늘의 답변을 써주세요."
+	
+	@IBOutlet weak var questionView: UIView!
+	@IBOutlet weak var questionLabel: UILabel!
+	@IBOutlet weak var answerView: UIView!
+	@IBOutlet weak var answerTextView: UITextView!
+	@IBOutlet weak var fenceImageView: UIImageView!
+	@IBOutlet weak var finishAnswerView: UIView!
+	@IBOutlet weak var finishAnswerButton: UIButton!
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+		setUI()
+		setAnswerTextView()
     }
-    
 
-    /*
-    // MARK: - Navigation
+	private func setUI(){
+		questionView.backgroundColor = UIColor(red: 0.733, green: 0.82, blue: 0.741, alpha: 0.6)
+		questionView.cornerRadius = 12
+		answerView.backgroundColor = UIColor(red: 0.733, green: 0.82, blue: 0.741, alpha: 0.6)
+		answerView.cornerRadius = 12
+		finishAnswerView.cornerRadius = 25
+	}
+	
+	private func setAnswerTextView(){
+		answerTextView.text = textViewPlaceHolder
+		answerTextView.textColor = UIColor(named: "AnswerPlaceHolder")
+		answerTextView.backgroundColor = .clear
+		answerTextView.delegate = self
+	}
+	
+	@objc private func didTapTextView(_ sender: Any) {
+		view.endEditing(true)
+	}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+}
 
+extension AnswerQuestionViewController: UITextViewDelegate {
+	func textViewDidBeginEditing(_ textView: UITextView) {
+		if textView.text == textViewPlaceHolder {
+			textView.text = nil
+			textView.textColor = UIColor(named: "QuestionLabel")
+		}
+	}
+}
+
+extension UILabel {
+	func asColor(targetString: String, color: UIColor?) {
+		let fullText = text ?? ""
+		let range = (fullText as NSString).range(of: targetString)
+		let attributedString = NSMutableAttributedString(string: fullText)
+		attributedString.addAttribute(.foregroundColor, value: color as Any, range: range)
+		attributedText = attributedString
+	}
 }
