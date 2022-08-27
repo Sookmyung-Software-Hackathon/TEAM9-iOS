7a815d4e44d00f6b5a672d60cca3317ff45703db
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-08-27 22:15:30 +0900

    Initial뷰 alert 추가

--- Farmily/Farmily/Screen/Initial/InitialViewController.swift
+++ Farmily/Farmily/Screen/Initial/InitialViewController.swift
@@ -7,10 +7,14 @@
 
 import UIKit
 
-class InitialViewController: UIViewController {
+final class InitialViewController: UIViewController {
+    
+    // MARK: - IBOutlet
 
     @IBOutlet weak var newFarmilyButton: UIButton!
     @IBOutlet weak var joinFarmilyButton: UIButton!
+    
+    // MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,33 +22,57 @@ class InitialViewController: UIViewController {
         setUI()
     }
     
+    // MARK: - Function (UI)
+    
     private func setUI() {
         [newFarmilyButton, joinFarmilyButton].forEach({ button in
             button?.cornerRadius = 25
         })
     }
     
+    // MARK: - IBAction
     
-
-//    @IBAction func showAlertButtonDIdTap(_ sender: Any) {
-//        makeTextFieldOKAlert()
-//    }
+    @IBAction func newFarmilyButtonDidTap(_ sender: Any) {
+        
+        self.makeOKAlert(title: "가족 코드가 복사되었습니다.",
+                         message: "코드: D3LEk9", okAction: { _ in
+            print("화면전환")
+        }, completion: nil)
+    }
+    
+    @IBAction func joinFarmilyButtonDidTap(_ sender: Any) {
+        makeTextFieldRequestAlert {
+            print("화면전환")
+        }
+    }
 }
 
 extension InitialViewController {
     
-    func makeTextFieldOKAlert(completion: (() -> Void)? = nil) {
-        let alertViewController = UIAlertController(title: title ?? nil,
-                                                    message: "message",
+    /// 취소와 확인이 뜨는 UIAlertController
+    func makeTextFieldRequestAlert(completion : (() -> Void)? = nil)
+    {
+        
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.impactOccurred()
+        
+        let alertViewController = UIAlertController(title: nil, message: "가족 코드를 입력해주세요",
                                                     preferredStyle: .alert)
+        
         alertViewController.addTextField { (textField) in
             textField.placeholder = "코드입력"
         }
+        
         let okAction = UIAlertAction(title: "확인", style: .default, handler: { _ in
             /// 통신
             print(alertViewController.textFields?[0].text ?? "nil")
         })
         alertViewController.addAction(okAction)
+        
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        alertViewController.addAction(cancelAction)
+        
         
         self.present(alertViewController, animated: true, completion: completion)
     }
