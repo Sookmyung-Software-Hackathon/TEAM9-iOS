5a71fdff0e6b2c583d2960f066125565a9351a64
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-08-27 16:27:57 +0900

    하단 탭바 생성

--- Farmily/Farmily/Screen/MainTabBarController.swift
+++ Farmily/Farmily/Screen/MainTabBarController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabBarController.swift
 //  Farmily
 //
 //  Created by 김혜수 on 2022/08/27.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
-
