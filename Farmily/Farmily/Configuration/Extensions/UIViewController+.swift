38adf5e3178a97064d55b78107d980daf052dba2
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-08-27 21:03:15 +0900

    UIViewController extension 추가

---
+++ Farmily/Farmily/Configuration/Extensions/UIViewController+.swift
@@ -0,0 +1,53 @@
+//
+//  UIViewController+.swift
+//  Farmily
+//
+//  Created by 김혜수 on 2022/08/27.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    /// 취소와 확인이 뜨는 UIAlertController
+    func makeRequestAlert(title : String,
+                          message : String,
+                          okAction : ((UIAlertAction) -> Void)?,
+                          cancelAction : ((UIAlertAction) -> Void)? = nil,
+                          completion : (() -> Void)? = nil)
+    {
+        
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.impactOccurred()
+        
+        let alertViewController = UIAlertController(title: title, message: message,
+                                                    preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: okAction)
+        alertViewController.addAction(okAction)
+        
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: cancelAction)
+        alertViewController.addAction(cancelAction)
+        
+        
+        self.present(alertViewController, animated: true, completion: completion)
+    }
+    
+    /// 확인 버튼만 뜨는 UIAlertController
+    func makeOKAlert(title: String?,
+                     message: String,
+                     okAction: ((UIAlertAction) -> Void)? = nil,
+                     completion: (() -> Void)? = nil) {
+        let alertViewController = UIAlertController(title: title ?? nil,
+                                                    message: message,
+                                                    preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: okAction)
+        alertViewController.addAction(okAction)
+        
+        self.present(alertViewController, animated: true, completion: completion)
+    }
+    
+}
+
