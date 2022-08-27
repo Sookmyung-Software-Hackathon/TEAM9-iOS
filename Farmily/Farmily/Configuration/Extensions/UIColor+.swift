6d6746d0d64937ef5320dfaf0add3a7de20c6cb2
hellozo0 <ahju0011@gmail.com>
2022-08-28 06:32:00 +0900

    [CHORE] 탭바 높이가 안바뀌어요 -

--- Farmily/Farmily/Configuration/Extensions/UIColor+.swift
+++ Farmily/Farmily/Configuration/Extensions/UIColor+.swift
@@ -15,5 +15,8 @@ extension UIColor {
     @nonobjc class var RecordListGreen37: UIColor {
         return UIColor(red: 0.733, green: 0.820, blue: 0.741, alpha: 0.37)
     }
+	@nonobjc class var TabBarGreen: UIColor {
+		return UIColor(red: 0.733, green: 0.820, blue: 0.741, alpha: 1)
+	}
 }
 
--- Farmily/Farmily/Screen/MainTabBarController.swift
+++ Farmily/Farmily/Screen/MainTabBarController.swift
@@ -8,9 +8,34 @@
 import UIKit
 
 class MainTabBarController: UITabBarController {
-
+	
+	fileprivate lazy var defaultTabBarHeight = { self.tabBar.frame.size.height }()
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-
+		setTabBarUI()
     }
+	
+	override func viewWillLayoutSubviews() {
+		super.viewWillLayoutSubviews()
+
+		let newTabBarHeight = defaultTabBarHeight + 16.0
+
+		var newFrame = self.tabBar.frame
+		newFrame.size.height = newTabBarHeight
+		newFrame.origin.y = view.frame.size.height - newTabBarHeight
+
+		self.tabBar.frame = newFrame
+	}
+	
+	private func setTabBarUI() {
+		self.tabBar.tintColor = UIColor.TabBarGreen
+		self.tabBar.backgroundColor = UIColor.white
+		self.tabBar.layer.cornerRadius = 12
+		self.tabBar.layer.masksToBounds = true
+		self.tabBar.layer.borderColor = UIColor.TabBarGreen.cgColor
+		self.tabBar.layer.borderWidth = 0.3 //tabbar 테두리 굵기
+		self.tabBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+	}
+
 }
