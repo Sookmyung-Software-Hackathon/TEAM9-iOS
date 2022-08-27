f3a1a1113e27d0dd51cc43731872226575f62452
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-08-27 23:44:31 +0900

    셀 파일 추가

---
+++ Farmily/Farmily/Screen/QuestionList/Cells/WeekPhotoCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  WeekPhotoCollectionViewCell.swift
+//  Farmily
+//
+//  Created by 김혜수 on 2022/08/27.
+//
+
+import UIKit
+
+class WeekPhotoCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Farmily/Farmily/Screen/QuestionList/Cells/WeekQuestionCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  WeekQuestionCollectionViewCell.swift
+//  Farmily
+//
+//  Created by 김혜수 on 2022/08/27.
+//
+
+import UIKit
+
+class WeekQuestionCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
