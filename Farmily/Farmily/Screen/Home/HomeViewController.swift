550440c80ea8e08c9e177e734e436255056dc5a3
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-08-28 14:30:59 +0900

    오늘질문 연결

--- Farmily/Farmily/Screen/Home/HomeViewController.swift
+++ Farmily/Farmily/Screen/Home/HomeViewController.swift
@@ -7,24 +7,49 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+import RxSwift
+
+final class HomeViewController: UIViewController {
+    
+    private let disposeBag = DisposeBag()
+    
+    var isAnswered: Bool?
 
 	@IBOutlet weak var questionLabel: UILabel!
 	
 	override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        getTodayQuestion()
     }
     
 	@IBAction func pressToGoAnswerQuestionView(_ sender: Any) {
 		
-		let storyboard = UIStoryboard(name: "AnswerQuestion", bundle : nil)
-												 
-		guard let answerQuestionVC = storyboard.instantiateViewController(withIdentifier: "AnswerQuestionViewController") as? AnswerQuestionViewController else {return}
-		answerQuestionVC.modalPresentationStyle = .fullScreen
-		self.present(answerQuestionVC, animated: true, completion: nil)
+        if let _ = isAnswered {
+            let storyboard = UIStoryboard(name: "AnswerQuestion", bundle : nil)
+                                                     
+            guard let answerQuestionVC = storyboard.instantiateViewController(withIdentifier: "AnswerQuestionViewController") as? AnswerQuestionViewController else {return}
+            answerQuestionVC.modalPresentationStyle = .fullScreen
+            self.present(answerQuestionVC, animated: true, completion: nil)
+        }
+        else {
+            self.makeOKAlert(title: nil, message: "이미 답변한 질문입니다.")
+        }
 	}
 	
 
+}
+
+// MARK: - Extension (Network)
+extension HomeViewController {
+    
+    private func getTodayQuestion() {
+        NetworkService.shared.question.getTodayQuestion()
+            .compactMap { $0.data }
+            .bind {
+                self.questionLabel.text = $0.question
+                self.isAnswered = $0.isAnswered
+            }
+            .disposed(by: disposeBag)
+    }
 }
