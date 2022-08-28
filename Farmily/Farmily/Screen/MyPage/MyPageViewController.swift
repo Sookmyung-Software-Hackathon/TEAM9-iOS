a57f30243129484a11aec6e6b81d686b1a3a1c0d
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-08-28 15:49:25 +0900

    앨범

--- Farmily/Farmily/Screen/MyPage/MyPageViewController.swift
+++ Farmily/Farmily/Screen/MyPage/MyPageViewController.swift
@@ -7,23 +7,95 @@
 
 import UIKit
 
-class MyPageViewController: UIViewController {
+import SnapKit
+import Then
 
+final class ImageCollectionCell: UICollectionViewCell {
+    
+    static let identifier = "ImageCollectionCell"
+    
+    private let imageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setData(image: String) {
+        imageView.image(url: image)
+    }
+    
+    private func setLayout() {
+        
+        addSubviews([imageView])
+        
+        imageView.snp.makeConstraints {
+            $0.top.leading.bottom.trailing.equalToSuperview()
+        }
+    }
+}
+
+final class MyPageViewController: UIViewController {
+    
+    var photos: [String]?
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setCollectionView()
     }
     
+    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension MyPageViewController {
+    
+    private func setCollectionView() {
+        registerCell()
+        collectionView.setCollectionViewLayout(createLayout(), animated: true)
+        collectionView.dataSource = self
     }
-    */
+    
+    private func registerCell() {
+        collectionView.register(ImageCollectionCell.self,
+                                forCellWithReuseIdentifier: ImageCollectionCell.identifier)
+    }
+    
+    private func createLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1/3),
+            heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalWidth(1/3))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+}
 
+extension MyPageViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCollectionCell.identifier, for: indexPath) as? ImageCollectionCell else { return UICollectionViewCell() }
+        cell.setData(image: "https://sopt-seminar.s3.ap-northeast-2.amazonaws.com/1661664028408_defautlt.png")
+        return cell
+    }
+    
+    
+    
+    
 }
