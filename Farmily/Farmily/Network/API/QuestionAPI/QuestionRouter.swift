6022adacdd9926446d73a3a83b344bcc3099c605
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-08-28 12:50:30 +0900

    매주 사진 추가 API 작성

--- Farmily/Farmily/Network/API/QuestionAPI/QuestionRouter.swift
+++ Farmily/Farmily/Network/API/QuestionAPI/QuestionRouter.swift
@@ -11,6 +11,11 @@ struct FamilyQuestionAddRequest: Codable {
     let question: String
 }
 
+struct PostFamilyPhotoRequest {
+    let file: UIImage
+    let week: Int
+}
+
 
 enum QuestionRouter {
     /// 오늘의 질문 조회
@@ -19,14 +24,14 @@ enum QuestionRouter {
     /// 오늘의 질문에 답변
     case postAnswerTodayQuestion
     
-    /// 가족 질문 추가
+    /// 가족 질문 추가 (DONE)
     case postFamilyQuestionAdd(question: FamilyQuestionAddRequest)
     
     /// 마지막 주차 조회 (DONE)
     case getQuestionLastWeek
     
     /// 해당 주에 사진 추가
-    case postPhoto
+    case postPhoto(familyPhoto: PostFamilyPhotoRequest)
     
     /// 해당 주 질문 조회 (DONE)
     case getWeekQuestion(week: Int)
@@ -44,6 +49,8 @@ extension QuestionRouter: BaseTargetType {
             return URLConstant.questionList + "/\(week)"
         case .postFamilyQuestionAdd:
             return URLConstant.questionFamily
+        case let .postPhoto(familyPhoto):
+            return URLConstant.questionList + "/\(familyPhoto.week)"
         default:
             return ""
         }
@@ -57,6 +64,8 @@ extension QuestionRouter: BaseTargetType {
             return .get
         case .postFamilyQuestionAdd:
             return .post
+        case .postPhoto:
+            return .post
         default:
             return .get
         }
@@ -69,10 +78,25 @@ extension QuestionRouter: BaseTargetType {
         case let .postFamilyQuestionAdd(question):
             return .requestParameters(parameters: question.toDictionary,
                                       encoding: JSONEncoding.default)
+        case let .postPhoto(familyPhoto):
+            return .uploadMultipart(createMultipartFormDataList(familyPhoto))
         default:
             return .requestPlain
         }
     }
     
   
+}
+
+extension QuestionRouter {
+    
+    func createMultipartFormDataList(_ postFamilyPhoto: PostFamilyPhotoRequest) -> [MultipartFormData] {
+            var list: [MultipartFormData] = []
+            list.append(MultipartFormData(
+                provider: .data(postFamilyPhoto.file.pngData() ?? Data()),
+                name: "file",
+                fileName: "defautlt.png",
+                mimeType: "image/png"))
+            return list
+        }
 }
---
+++ Farmily/Farmily/Network/Models/FamilyPhoto.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyPhoto.swift
+//  Farmily
+//
+//  Created by 김혜수 on 2022/08/28.
+//
+
+import Foundation
+
+struct FamilyPhoto: Decodable {
+    
+    let id: Int
+    let url: String
+    let week: Int
+    let familyId: Int
+    let createdAt: String
+}
