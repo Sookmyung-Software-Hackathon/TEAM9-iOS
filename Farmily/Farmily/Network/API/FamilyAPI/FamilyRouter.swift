61f18e189c159ce009a9119583600c8cde6ee097
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-08-28 10:22:04 +0900

    family network 작성

--- Farmily/Farmily/Network/API/FamilyAPI/FamilyRouter.swift
+++ Farmily/Farmily/Network/API/FamilyAPI/FamilyRouter.swift
@@ -5,5 +5,40 @@
 //  Created by 김혜수 on 2022/08/27.
 //
 
-import Foundation
+import Moya
 
+struct FamilyJoinRequest: Encodable {
+    let device: String
+    let code: String
+}
+
+enum FarmilyRouter {
+    case postFamily
+    case postFamilyJoin(joinRequest: FamilyJoinRequest)
+}
+
+extension FarmilyRouter: BaseTargetType {
+    var path: String {
+        switch self {
+        case .postFamily:
+            return URLConstant.family
+        case .postFamilyJoin:
+            return URLConstant.familyJoin
+        }
+    }
+    
+    var method: Method {
+        return .post
+    }
+    
+    var task: Task {
+        switch self {
+        case .postFamily:
+            return .requestPlain
+        case let .postFamilyJoin(request):
+            return .requestParameters(
+                parameters: request.toDictionary,
+                encoding: JSONEncoding.default)
+        }
+    }
+}
